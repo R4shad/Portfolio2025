f9cb67de08f42e945c6af7725d740b5eeb483f5f
Amir Barra <barra.amir14@gmail.com>
2025-03-16 18:36:58 -0400

    feat: Header created and styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Main from './Componets/Main/Main'
+import Header from './Componets/Header/Header'
 
 function App() {
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setShowNavbar(window.scrollY > 340)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
     <div className="app-container">
+      {showNavbar && <Header />}
       <Main />
     </div>
   )
---
+++ src/Componets/Header/Header.tsx
@@ -0,0 +1,42 @@
+import './Header.css'
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa'
+
+const Header = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <img className="img-perfil-navbar" src="/perfil.JPG" alt="Profile" />
+        <div className="navbar-text">
+          <h1>Amir Barra - Systems Engineer</h1>
+          <h2>Web Developer and IT</h2>
+        </div>
+      </div>
+
+      <div className="navbar-icons">
+        <a
+          href="https://github.com/tuusuario"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="icon github" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/tuusuario"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="icon linkedin" />
+        </a>
+        <a
+          href="https://www.instagram.com/tuusuario"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram className="icon instagram" />
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
